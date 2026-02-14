3c068cbbcc820f9d0ea5976f92b5f9265f6b13dc
Hector Mcafee//UP2006081 <78622640+Hector-McAfee@users.noreply.github.com>
2026-02-14 03:07:30 +0000

    pendings

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -12,6 +12,7 @@ import * as start_bingo from "./setup/start_bingo.js";
 import * as open_verification from "./setup/open_verification.js";
 import * as end_bingo from "./setup/end_bingo.js";
 import * as manual_chest_verify from "./setup/manual_chest_verify.js";
+import * as list_pending_drops from "./setup/list_pending_drops.js";
 
 export const minimal: RESTPostAPIApplicationCommandsJSONBody[] = [
   bingo_create.data.toJSON()
@@ -36,6 +37,7 @@ export const full: RESTPostAPIApplicationCommandsJSONBody[] = [
   start_bingo.data.toJSON(),
   open_verification.data.toJSON(),
   manual_chest_verify.data.toJSON(),
+  list_pending_drops.data.toJSON(),
   end_bingo.data.toJSON()
 ];
 
@@ -58,5 +60,6 @@ export const handlers = {
   start_bingo: start_bingo.execute,
   open_verification: open_verification.execute,
   manual_chest_verify: manual_chest_verify.execute,
+  list_pending_drops: list_pending_drops.execute,
   end_bingo: end_bingo.execute
 } as const;
